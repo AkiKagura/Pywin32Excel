4b06be9f812d37ad2020590008492f1e6fcaadfe
AkiKagura <49899720+AkiKagura@users.noreply.github.com>
2021-04-01 19:05:54 +0800

    Update public_func.py

--- PyWin32Excel/public_func.py
+++ PyWin32Excel/public_func.py
@@ -13,3 +13,22 @@ def write_list(txt_path, lst):
         f.write(var)
     f.close()
 
+
+def get_list(txt_path):
+    res = ()
+    f = open(txt_path, 'r', encoding='UTF-8')
+    for line in f:
+        if line[0] == '[' or line[0] == '\n':
+            continue
+        res += (line.split(','),)
+    return res
+
+
+def dic_compare_lst(dic, list_to_search):
+    list_output = []
+    for item in list_to_search:
+        if item in dic:
+            list_output.append(dic[item])
+        else:
+            list_output.append('[NONE]')
+    return list_output
